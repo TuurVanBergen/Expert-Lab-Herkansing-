a56ced3ce33de4d5b68c992d8d031c72c4db8253
TuurVanBergen <tuur.van.bergen@student.ehb.be>
2025-08-10 13:01:31 +0200

    fix(QuestionPage): new start

--- vragenlijst/src/pages/QuestionPage.jsx
+++ vragenlijst/src/pages/QuestionPage.jsx
@@ -1,15 +1,15 @@
-import "../styles/QuestionPage.css";
 import React from "react";
+
 function QuestionPage({ question, onAnswer }) {
 	return (
 		<div className="question-page">
 			<div className="question-box">
 				<p className="question-text">{question.text}</p>
 				<div className="button-group">
-					<button className="yes-btn" onClick={() => onAnswer("yes")}>
+					<button className="yes-btn" onClick={() => onAnswer("ja")}>
 						Ja
 					</button>
-					<button className="no-btn" onClick={() => onAnswer("no")}>
+					<button className="no-btn" onClick={() => onAnswer("nee")}>
 						Nee
 					</button>
 				</div>
